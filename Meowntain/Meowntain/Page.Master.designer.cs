da646a41da8478672b52d517de21ce3162fbc0d7
Alex Harrison <Saphix@ymail.com>
2017-04-21 20:42:10 -0500

    Added plain template to master page

--- Meowntain/Meowntain/Page.Master.designer.cs
+++ Meowntain/Meowntain/Page.Master.designer.cs
@@ -13,15 +13,6 @@ namespace Meowntain {
     public partial class Page {
         
         /// <summary>
-        /// head control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -29,14 +20,5 @@ namespace Meowntain {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
