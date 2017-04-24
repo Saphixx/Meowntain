f9239b6704a88edae50e299cd05890c8506c5b57
easibley <easibley@gmail.com>
2017-04-23 21:57:52 -0500

    Add files via upload
    CatInventory / Scheduler pages.
    
    - connection strings will need to be redone for local machine DB
    - static user key used in Schedule SQL Insert Query (no created user data present and foreign key required)
    - Appointments table needs to be updated to have datetime attribute instead of date
    - phonenumber as int attribute in user table results in overflow

---
+++ RequestAppointment.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Meowntain
+{
+    public partial class WebForm5 : System.Web.UI.Page
+    {
+        Boolean submitted = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            txtDateTime.Text = DateTime.Now.ToString("yyyy-MM-ddTHH:mm");
+
+            HttpCookie cookie_cat = Request.Cookies["CatSelect"];
+
+            DataTable dt = new DataTable();
+
+            
+            string strQuery = "select * from [Cat] where CatID = @Name order by CatID ";
+            SqlCommand cmd = new SqlCommand(strQuery);
+            cmd.Parameters.AddWithValue("@Name", cookie_cat["catID"]);
+            SqlConnection con = new SqlConnection(SqlDataSource1.ConnectionString);
+            SqlDataAdapter sda = new SqlDataAdapter();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                sda.SelectCommand = cmd;
+                sda.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+                sda.Dispose();
+                con.Dispose();
+            }
+
+        }
+
+
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid && !submitted)
+            {
+                HttpCookie cookie_cat = Request.Cookies["CatSelect"];
+                char[] delmintier = new char[] { '.' };
+                string dateTime = txtDateTime.Text.Replace("T", " ").Split(delmintier)[0];
+
+                SqlConnection con = new SqlConnection(SqlDataSource1.ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO [Appointment](User_FK, Cat_FK, Date) VALUES ('2', '"+ cookie_cat["catID"] + "','" + dateTime + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
